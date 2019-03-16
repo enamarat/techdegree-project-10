df1abc704e2702ea07dfff02d6b0fe51fc65a71c
Marat Enaliev <enamarat@yandex.ru>
2019-03-16 18:56:51 +0300

    Declare routes.

---
+++ routes/books.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET books listing. */
+router.get('/', function(req, res, next) {
+  res.render('index.pug', { title: 'Books' });
+});
+
+/* Add a new book form. */
+router.get('/books/new', function(req, res, next) {
+  res.render('new-book.pug', { title: 'Books' });
+});
+
+/* Post a new book to database. */
+router.post('/books/new', function(req, res, next) {
+  res.render('new-book.pug', { title: 'Books' });
+});
+
+/* Show book details form. */
+router.get('/books/:id', function(req, res, next) {
+  res.render('update-book.pug', { title: 'Books' });
+});
+
+/* Update book info in the database. */
+router.post('/books/:id', function(req, res, next) {
+  res.render('update-book.pug', { title: 'Books' });
+});
+
+/* Delete a book. */
+router.post('/books/:id/delete', function(req, res, next) {
+  res.render('update-book.pug', { title: 'Books' });
+});
+
+
+/* Function which handles non-existenet routes. */
+app.use((request, response, next) => {
+  const error = new Error('Page not found');
+  error.status = 404;
+  //next(error);
+  response.render('page-not-found.pug', {
+    error: error,
+    errorStatus: error.status
+  })
+})
+
+/*Error handler*/
+app.use((error, request, response, next) => {
+  response.locals.error = error;
+  response.status(error.status);
+  response.render('error.pug', {
+    error: error,
+    errorStatus: error.status
+  })
+})
+
+module.exports = router;
