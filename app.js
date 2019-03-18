ee3af475a94255e941edebe269d8b4f1a0f5d8b2
Marat Enaliev <enamarat@yandex.ru>
2019-03-18 20:07:33 +0300

    Add and delete entries.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
 var booksRouter = require('./routes/books');
@@ -10,18 +12,24 @@ var booksRouter = require('./routes/books');
 var app = express();
 
 
+// Setting directory where ststic files are kept
+app.use('/static', express.static('public'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/books.js
+++ routes/books.js
@@ -1,41 +1,53 @@
 var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
+
 const app = express();
 
 
 /* GET books listing. */
 router.get('/', function(req, res, next) {
-  Book.findAll().then( (books) => {
-    res.render('index.pug', { books: books, title: 'Books' });
+  Book.findAll().then((books) => {
+    res.render('index.pug', { books: books, pageTitle: 'Books'});
+  });
+});
+
+/* Show book details form. */
+router.get('/details/:id', function(req, res, next) {
+  Book.findByPk(req.params.id).then((book) => {
+    res.render('update-book.pug', { book: book, pageTitle: 'Book details'});
+  });
+});
+
+/* Update book info in the database. */
+router.put('/details/:id', function(req, res, next) {
+  Book.findByPk(req.params.id).then((book) => {
+    return book.update(req.body);
+  }).then((book)=> {
+    res.redirect("/books");
   });
 });
 
 /* Add a new book form. */
-router.get('/books/new', function(req, res, next) {
-  res.render('new-book.pug', { title: 'Books' });
+router.get('/new', function(req, res, next) {
+  res.render('new-book.pug', { pageTitle: 'New Book' });
 });
 
 /* Post a new book to database. */
-router.post('/books/new', function(req, res, next) {
-  res.render('new-book.pug', { title: 'Books' });
-});
-
-/* Show book details form. */
-router.get('/books/:id', function(req, res, next) {
-  res.render('update-book.pug', { title: 'Books' });
-});
-
-/* Update book info in the database. */
-router.post('/books/:id', function(req, res, next) {
-  res.render('update-book.pug', { title: 'Books' });
+router.post('/new', function(req, res, next) {
+  return Book.create(req.body).then(() => {
+    res.redirect("/books");
+  });
 });
 
 /* Delete a book. */
-router.post('/books/:id/delete', function(req, res, next) {
-  res.render('update-book.pug', { title: 'Books' });
+router.post('/details/:id/delete', function(req, res, next) {
+  Book.findByPk(req.params.id).then((book)=>{
+    return book.destroy();
+  }).then(() => {
+    res.redirect("/books");
+  });
 });
-
 
 /* Function which handles non-existenet routes. */
 // app.use((request, response, next) => {
